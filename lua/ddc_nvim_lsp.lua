9d5bfb7e25ca1bd7b4f0f0ff227ceced63265d59
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-10 19:09:43 +0900

    Improve refresh

--- lua/ddc_nvim_lsp.lua
+++ lua/ddc_nvim_lsp.lua
@@ -1,12 +1,16 @@
 local api = vim.api
 
 local get_candidates = function(_, _, result)
-  local success = (type(result) == 'table' and not vim.tbl_isempty(result)) and true or false
+  local success = (type(result) == 'table' and not vim.tbl_isempty(result)
+    ) and true or false
   result = result and result['items'] ~= nil and result['items'] or result
-  api.nvim_set_var('ddc#source#lsp#_results', result)
-  api.nvim_set_var('ddc#source#lsp#_success', success)
-  api.nvim_set_var('ddc#source#lsp#_requested', true)
-  api.nvim_call_function('ddc#refresh_candidates', {})
+
+  if #result > 0 then
+    api.nvim_set_var('ddc#source#lsp#_results', result)
+    api.nvim_set_var('ddc#source#lsp#_success', success)
+    api.nvim_set_var('ddc#source#lsp#_requested', true)
+    api.nvim_call_function('ddc#refresh_candidates', {})
+  end
 end
 
 local request_candidates = function(arguments)
