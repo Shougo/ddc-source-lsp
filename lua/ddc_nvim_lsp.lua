f819073bd7bcd561d61777134625cb884a471e91
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-08-13 16:32:47 +0900

    Check nil value

--- lua/ddc_nvim_lsp.lua
+++ lua/ddc_nvim_lsp.lua
@@ -5,7 +5,7 @@ local get_candidates = function(_, _, result)
     ) and true or false
   result = result and result['items'] ~= nil and result['items'] or result
 
-  if #result > 0 then
+  if result and #result > 0 then
     api.nvim_set_var('ddc#source#lsp#_results', result)
     api.nvim_set_var('ddc#source#lsp#_success', success)
     api.nvim_set_var('ddc#source#lsp#_requested', true)
