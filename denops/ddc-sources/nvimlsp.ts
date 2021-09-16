98372d6c46299a3008f0caa2d27d8da27632fab1
HARUYAMA Seigo <haruyama@tankyu.net>
2021-09-16 23:32:46 +0900

    Use the CompletionItem from vscode_languageserver_types

--- denops/ddc-sources/nvimlsp.ts
+++ denops/ddc-sources/nvimlsp.ts
@@ -10,6 +10,9 @@ import {
   Denops,
   vars,
 } from "https://deno.land/x/ddc_vim@v0.5.0/deps.ts#^";
+import {
+  CompletionItem,
+} from "https://deno.land/x/vscode_languageserver_types@v0.1.0/mod.ts"
 
 const LSP_KINDS = [
   "Text",
@@ -97,10 +100,7 @@ export class Source extends BaseSource {
     const results = await vars.g.get(
       denops,
       "ddc#source#lsp#_results",
-    ) as Record<
-      string,
-      unknown
-    >[];
+    ) as CompletionItem[];
 
     if (results.length == 0) {
       return [];
@@ -118,24 +118,21 @@ export class Source extends BaseSource {
     const candidates = results.map((v) => {
       let word = "";
 
-      if ("textEdit" in v && v["textEdit"]) {
-        const textEdit = v["textEdit"] as Record<string, any>;
-        if (
-          textEdit && "range" in textEdit &&
-          textEdit.range.start == textEdit.range.end
-        ) {
+      if (v.textEdit) {
+        const textEdit = v.textEdit
+        if ("range" in textEdit && textEdit.range.start == textEdit.range.end) {
           word = `${previousInput.slice(completePosition)}${textEdit.newText}`;
         } else {
           word = textEdit.newText;
         }
-      } else if ("insertText" in v) {
-        if ("insertText" in v && v.insertTextFormat != 1) {
-          word = ("entryName" in v ? v.entryName : v.label) as string;
+      } else if (v.insertText) {
+        if (v.insertTextFormat != 1) {
+          word = v.label
         } else {
-          word = v.insertText as string;
+          word = v.insertText
         }
       } else {
-        word = ("entryName" in v ? v.entryName : v.label) as string;
+        word = v.label
       }
 
       // Remove parentheses from word.
@@ -163,16 +160,13 @@ export class Source extends BaseSource {
       }
 
       if (v.detail) {
-        item.menu = v.detail as string;
+        item.menu = v.detail;
       }
 
       if (typeof v.documentation === "string") {
         item.info = v.documentation;
-      } else if (
-        v.documentation && typeof v.documentation === "object" &&
-        "value" in v.documentation
-      ) {
-        item.info = (v.documentation as any).value;
+      } else if (v.documentation && "value" in v.documentation) {
+        item.info = v.documentation.value;
       }
 
       return item;
