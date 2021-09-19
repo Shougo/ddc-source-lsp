aff43f668e72bc0ea99092aabee8549a8ccfb62b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-09-20 00:45:47 +0900

    Fix for the latest ddc

--- denops/@ddc-sources/nvimlsp.ts
+++ denops/@ddc-sources/nvimlsp.ts
@@ -1,15 +1,15 @@
 import {
   BaseSource,
   Candidate,
-} from "https://deno.land/x/ddc_vim@v0.12.0/types.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.13.0/types.ts#^";
 import {
   GatherCandidatesArguments,
-} from "https://deno.land/x/ddc_vim@v0.12.0/base/source.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.13.0/base/source.ts#^";
 import {
   batch,
   Denops,
   vars,
-} from "https://deno.land/x/ddc_vim@v0.12.0/deps.ts#^";
+} from "https://deno.land/x/ddc_vim@v0.13.0/deps.ts#^";
 import {
   CompletionItem,
 } from "https://deno.land/x/vscode_languageserver_types@v0.1.0/mod.ts#^";
