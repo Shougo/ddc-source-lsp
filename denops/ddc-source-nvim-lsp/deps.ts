c23047147f9f6375cd3192df70fd0f6085256895
uga-rosa <uga6603@gmail.com>
2023-06-26 01:04:06 +0900

    deps.ts

---
+++ denops/ddc-source-nvim-lsp/deps.ts
@@ -0,0 +1,18 @@
+export {
+  BaseSource,
+  type DdcGatherItems,
+  type Item,
+} from "https://deno.land/x/ddc_vim@v3.7.0/types.ts";
+export {
+  assertEquals,
+  type Denops,
+  fn,
+  vars,
+} from "https://deno.land/x/ddc_vim@v3.7.0/deps.ts";
+export type {
+  GatherArguments,
+  OnInitArguments,
+  OnCompleteDoneArguments,
+} from "https://deno.land/x/ddc_vim@v3.7.0/base/source.ts";
+
+export * as LSP from "npm:vscode-languageserver-types@3.17.3";
