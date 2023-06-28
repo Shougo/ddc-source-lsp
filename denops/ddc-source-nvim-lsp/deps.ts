574bdb41074b58bd77372bb79c101e1744befbed
uga-rosa <uga6603@gmail.com>
2023-06-29 01:02:32 +0900

    fmt

--- denops/ddc-source-nvim-lsp/deps.ts
+++ denops/ddc-source-nvim-lsp/deps.ts
@@ -4,10 +4,7 @@ export {
   type Item,
   type PumHighlight,
 } from "https://deno.land/x/ddc_vim@v3.7.0/types.ts";
-export {
-  type Denops,
-  fn,
-} from "https://deno.land/x/ddc_vim@v3.7.0/deps.ts";
+export { type Denops, fn } from "https://deno.land/x/ddc_vim@v3.7.0/deps.ts";
 export type {
   GatherArguments,
   OnCompleteDoneArguments,
