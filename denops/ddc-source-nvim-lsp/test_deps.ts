7aed3425446aed30b36add0124c572ca07bb8d96
uga-rosa <uga6603@gmail.com>
2023-06-29 05:30:46 +0900

    Remove unused export

--- denops/ddc-source-nvim-lsp/test_deps.ts
+++ denops/ddc-source-nvim-lsp/test_deps.ts
@@ -1,7 +1,6 @@
 export { assertEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts";
 
 export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export * as nvim from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
 
 export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
