6e6d06dbe6bd5086ac9df2ecbef3ba0d448376dc
uga-rosa <uga6603@gmail.com>
2023-06-28 18:18:20 +0900

    Fix test

--- denops/ddc-source-nvim-lsp/completion_item_test.ts
+++ denops/ddc-source-nvim-lsp/completion_item_test.ts
@@ -1,56 +1,80 @@
 import CompletionItem from "./completion_item.ts";
 import { assertEquals, LSP } from "./deps.ts";
+import { OffsetEncoding } from "./offset_encoding.ts";
 
-/**
- * 1|<div>
- * 2|  </d|>
- */
+const ClientId = 0 as const satisfies number;
+const OffsetEncoding = "utf-16" as const satisfies OffsetEncoding;
+const Resolvable = false as const satisfies boolean;
+
+function setup(args: {
+  input: string;
+  buffer: string[];
+  lspItem: LSP.CompletionItem;
+}) {
+  const line = args.buffer.findIndex((lineText) => lineText.includes("|"));
+  if (line === -1) {
+    throw new Error("Invalid buffer: cursor not found");
+  }
+  const completePos = args.buffer[line].indexOf("|");
+
+  const completionItem = new CompletionItem(
+    ClientId,
+    OffsetEncoding,
+    Resolvable,
+    args.buffer[line],
+    completePos,
+    completePos + args.input.length,
+  );
+  return completionItem.toDdcItem(args.lspItem);
+}
+
+function makeRange(
+  sl: number,
+  sc: number,
+  el: number,
+  ec: number,
+): LSP.Range {
+  return {
+    start: { line: sl, character: sc },
+    end: { line: el, character: ec },
+  };
+}
+
 Deno.test({
-  name: "toDdcItem",
+  name: "Indent fixing completion (vscode-html-language-server)",
   fn: () => {
     const buffer = [
       "<div>",
-      "  </d>",
+      "  </|foo>",
     ];
-    const cursorPos: LSP.Position = {
-      line: 1,
-      character: 5,
-    };
-    const clientId = 0;
-    const offsetEncoding = "utf-16";
-    const resolvable = false;
-    const completionItem = new CompletionItem(
-      clientId,
-      offsetEncoding,
-      resolvable,
-      buffer[1],
-      cursorPos.character - 1,
-    );
-
     const lspItem = {
       label: "/div",
       textEdit: {
-        range: {
-          start: { character: 0, line: 1 },
-          end: { character: 5, line: 1 },
-        },
+        range: makeRange(1, 0, 1, 3),
         newText: "</div",
       },
       kind: 10, // Property
-      insertTextFormat: 1, // plaintext
       filterText: "  </div",
     } satisfies LSP.CompletionItem;
+    const ddcItem = setup({
+      input: "d",
+      buffer,
+      lspItem,
+    });
 
-    const ddcItem = completionItem.toDdcItem(lspItem);
     assertEquals(ddcItem, {
       word: "div",
       abbr: "/div",
       kind: CompletionItem.Kind[lspItem.kind],
+      highlights: undefined,
       user_data: {
         lspitem: JSON.stringify(lspItem),
-        clientId,
-        resolvable,
+        clientId: ClientId,
+        offsetEncoding: OffsetEncoding,
+        resolvable: Resolvable,
         lineOnRequest: buffer[1],
+        suggestCharacter: 4,
+        requestCharacter: 5,
       },
     });
   },
