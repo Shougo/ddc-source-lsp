3efbc6c9fe32e12020b3c943c25d1ddc92ab3602
kamecha <masakikameyama81@gmail.com>
2023-07-17 15:10:24 +0900

    apply highlight param to each item

--- denops/@ddc-filters/converter_kind_labels.ts
+++ denops/@ddc-filters/converter_kind_labels.ts
@@ -1,4 +1,5 @@
 import { BaseFilter, Item } from "https://deno.land/x/ddc_vim@v3.9.0/types.ts";
+import { byteLength } from "../ddc-source-nvim-lsp/completion_item.ts";
 
 type Params = {
   kindLabels: Record<string, string>;
@@ -11,10 +12,25 @@ export class Filter extends BaseFilter<Params> {
     items: Item[];
   }): Promise<Item[]> {
     const labels = args.filterParams.kindLabels;
+    const hlGrouns = args.filterParams.kindHlGroups;
     for (const item of args.items) {
+      // extract hl_group here before item.kind is overwritten
+      const hl_group = hlGrouns[item.kind ?? ""];
       item.kind = item.kind && item.kind in labels
         ? labels[item.kind]
         : item.kind;
+      if (!hl_group) continue;
+      const highlights = item.highlights ?? [];
+      item.highlights = [
+        ...highlights,
+        {
+          name: "lsp-kind-label",
+          type: "kind",
+          hl_group: hl_group,
+          col: 1,
+          width: byteLength(item.kind ?? ""),
+        },
+      ];
     }
     return Promise.resolve(args.items);
   }
