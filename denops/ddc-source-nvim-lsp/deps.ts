0b8b39ffd4f1c15fec401ee4a64ba2142ba1d3c7
uga-rosa <uga6603@gmail.com>
2023-08-07 16:25:54 +0900

    Update dependencies

--- denops/ddc-source-nvim-lsp/deps.ts
+++ denops/ddc-source-nvim-lsp/deps.ts
@@ -2,7 +2,7 @@ export {
   deadline,
   DeadlineError,
   deferred,
-} from "https://deno.land/std@0.193.0/async/mod.ts";
+} from "https://deno.land/std@0.197.0/async/mod.ts";
 
 export {
   BaseFilter,
@@ -10,19 +10,19 @@ export {
   type DdcGatherItems,
   type Item,
   type PumHighlight,
-} from "https://deno.land/x/ddc_vim@v3.9.0/types.ts";
+} from "https://deno.land/x/ddc_vim@v3.9.2/types.ts";
 export {
   type Denops,
   fn,
   op,
-} from "https://deno.land/x/ddc_vim@v3.9.0/deps.ts";
+} from "https://deno.land/x/ddc_vim@v3.9.2/deps.ts";
 export type {
   GatherArguments,
   OnCompleteDoneArguments,
-} from "https://deno.land/x/ddc_vim@v3.9.0/base/source.ts";
+} from "https://deno.land/x/ddc_vim@v3.9.2/base/source.ts";
 export type {
   FilterArguments,
-} from "https://deno.land/x/ddc_vim@v3.9.0/base/filter.ts";
+} from "https://deno.land/x/ddc_vim@v3.9.2/base/filter.ts";
 
 export { register } from "https://deno.land/x/denops_std@v5.0.1/lambda/mod.ts";
 
@@ -36,4 +36,4 @@ export {
   toUtf16Index,
 } from "https://deno.land/x/denops_lsputil@v0.5.4/mod.ts";
 
-export * as LSP from "npm:vscode-languageserver-types@3.17.4-next.0";
+export * as LSP from "npm:vscode-languageserver-types@3.17.3";
