32781461ac8fc85dfad77fce3b1200644c2eaf52
uga-rosa <uga6603@gmail.com>
2023-11-30 00:21:25 +0900

    Add module for request

--- denops/ddc-source-nvim-lsp/deps/denops.ts
+++ denops/ddc-source-nvim-lsp/deps/denops.ts
@@ -2,7 +2,10 @@ export type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
 export * as fn from "https://deno.land/x/denops_std@v5.0.1/function/mod.ts";
 export * as nvim from "https://deno.land/x/denops_std@v5.0.1/function/nvim/mod.ts";
 export * as op from "https://deno.land/x/denops_std@v5.0.1/option/mod.ts";
-export { register } from "https://deno.land/x/denops_std@v5.0.1/lambda/mod.ts";
+export {
+  register,
+  unregister,
+} from "https://deno.land/x/denops_std@v5.0.1/lambda/mod.ts";
 export { batch } from "https://deno.land/x/denops_std@v5.0.1/batch/mod.ts";
 
 export { test } from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
---
+++ denops/ddc-source-nvim-lsp/request.ts
@@ -0,0 +1,77 @@
+import { Denops, register, unregister } from "./deps/denops.ts";
+import { deadline, DeadlineError, deferred } from "./deps/std.ts";
+import { is, u } from "./deps/unknownutil.ts";
+import { Client, Params } from "../@ddc-sources/nvim-lsp.ts";
+import { LSP } from "./deps/lsp.ts";
+
+export async function request(
+  denops: Denops,
+  lspEngine: Params["lspEngine"],
+  method: "textDocument/completion" | "completionItem/resolve",
+  params: unknown,
+  opts: { client: Client; timeout: number },
+): Promise<unknown> {
+  if (lspEngine === "nvim-lsp") {
+    if (method === "textDocument/completion") {
+      const defer = deferred();
+      const id = register(denops, (response: unknown) => {
+        defer.resolve(response);
+      });
+      await denops.call(
+        `luaeval`,
+        `require("ddc_nvim_lsp.internal").request(_A[1], _A[2], _A[3])`,
+        [opts.client.id, params, { name: denops.name, id }],
+      );
+      return await deadline(defer, opts.timeout);
+    } else {
+      let lspItem = params;
+      lspItem = await denops.call(
+        "luaeval",
+        `require("ddc_nvim_lsp.internal").resolve(_A[1], _A[2])`,
+        [opts.client.id, lspItem],
+      ) as LSP.CompletionItem | null ?? lspItem;
+      return lspItem;
+    }
+  } else if (lspEngine === "vim-lsp") {
+    const data = deferred<unknown>();
+    const id = register(
+      denops,
+      (response: unknown) => data.resolve(response),
+    );
+    try {
+      await denops.eval(
+        `lsp#send_request(l:server, extend(l:request,` +
+          `{'on_notification': {data -> denops#notify(l:name, l:id, [data])}}))`,
+        {
+          server: opts.client.id,
+          request: { method, params },
+          name: denops.name,
+          id,
+        },
+      );
+      const resolvedData = await deadline(data, opts.timeout);
+      const { response } = u.ensure(resolvedData, is.Record);
+      const { result } = u.ensure(response, is.Record);
+      return result;
+    } catch (e) {
+      if (e instanceof DeadlineError) {
+        throw new Error(`No response from server ${opts.client.id}`);
+      } else {
+        throw new Error(`Unsupprted method: ${method}`);
+      }
+    } finally {
+      unregister(denops, id);
+    }
+  } else if (lspEngine === "lspoints") {
+    return await denops.dispatch(
+      "lspoints",
+      "request",
+      opts.client.id,
+      method,
+      params,
+    );
+  } else {
+    lspEngine satisfies never;
+    throw new Error(`unknown lspEngine: ${lspEngine}`);
+  }
+}
