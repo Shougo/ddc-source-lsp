beab48f26f0f18097a730f7c9bb419c165bfa0cf
atusy <30277794+atusy@users.noreply.github.com>
2024-02-22 09:10:39 +0900

    refactor: uriFromBufnr can be exported from the root mod of lsputil

--- denops/ddc-source-lsp/deps/lsp.ts
+++ denops/ddc-source-lsp/deps/lsp.ts
@@ -8,10 +8,7 @@ export {
   type OffsetEncoding,
   parseSnippet,
   toUtf16Index,
-} from "https://deno.land/x/denops_lsputil@v0.9.3/mod.ts";
-
-export {
   uriFromBufnr,
-} from "https://deno.land/x/denops_lsputil@v0.9.3/uri/mod.ts";
+} from "https://deno.land/x/denops_lsputil@v0.9.3/mod.ts";
 
 export * as LSP from "npm:vscode-languageserver-protocol@3.17.6-next.1";
